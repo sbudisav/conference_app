8208aa9e97f778d62f72ce51ada89372e39adc72
sbudisav <sbudisav@gmail.com>
2018-09-04 12:16:49 -0500

    adds be_cruddy homework

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+Speaker.create({first_name: "Dani", last_name: "Zaghian", email: "danielle@actualize.co"})
+Speaker.create({first_name: "Frost", last_name: "Borginstein", email:"letsgetitfrosted@altavista.com"})
